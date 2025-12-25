f9b69d97bb0cdac656c89c312ee103cb39406ce2
Max Halford <maxhalford25@gmail.com>
2025-12-25 15:13:32 +0100

    remove stuff

--- src/stores/settings.js
+++
@@ -1,8 +0,0 @@
-import { defineStore } from 'pinia'
-
-export const useSettingsStore = defineStore('settings', () => {
-  // Settings store can be used for future settings if needed
-  // Font settings have been removed - using opinionated defaults instead
-
-  return {}
-})
